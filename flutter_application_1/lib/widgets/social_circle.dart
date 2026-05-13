36f5c151efb6d99050fa2edf323fbe133b991520
Jay Andre Sasi <jayandre.sasi@wvsu.edu.ph>
2026-05-14 01:37:13 +0800

    Add SocialCircle widget for social icon buttons
    Implemented a reusable SocialCircle widget for social icon buttons.

---
+++ flutter_application_1/lib/widgets/social_circle.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../theme.dart';
+
+/// Reusable widget for social icon circle button
+class SocialCircle extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+
+  const SocialCircle({
+    Key? key,
+    required this.icon,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 48,
+      height: 48,
+      decoration: BoxDecoration(
+        color: AppColors.surface,
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        onPressed: () {},
+        icon: Icon(icon, color: color),
+      ),
+    );
+  }
+}
